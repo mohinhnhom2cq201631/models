209e2862587cccbb6a5267ec50f0bdcf421a14af
Luu Tuan Nguyen <miketuannguyen@gmail.com>
2020-01-03 18:43:39 +0700

    complete all

--- DAO/orderDAO.js
+++ DAO/orderDAO.js
@@ -10,3 +10,7 @@ exports.get_User_By_OrderID = async id =>{
     const order = await Order.findById(id,'user').populate('user');
     return await User.findById(order.user);
 }
+
+exports.get_Order_By_ID = id => {
+    return Order.findById(id).populate('user');
+}
